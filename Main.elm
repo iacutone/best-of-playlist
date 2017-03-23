e313452c9ddbb1b95d1d4525f7b732c84ecc4fd3
Eric Iacutone <eric.iacutone@gmail.com>
2017-03-22 22:13:49 -0400

    Set up initial Elm model

--- Main.elm
+++ Main.elm
@@ -1,9 +1,58 @@
 module Main exposing (..)
 
-import Html exposing (Html, span, text)
-import Html.Attributes exposing (class)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+-- import Native.Audio
+import Time
+import Debug
 
+-- MSG
 
-main : Html a
+type Msg
+    = NoOp
+
+-- MODEL
+
+type alias Model =
+    { mediaUrl : String
+    , playing: Bool
+    , duration: Float
+    }
+
+-- INIT
+
+initialModel : Model
+initialModel =
+    { mediaUrl = "songs/The Julie Ruin - I'm Done.webm"
+    , playing = False
+    , duration = 0.0
+    }
+
+-- UPDATE
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        _ ->
+            Debug.log "Unknown message" ( model, Cmd.none )
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div [ class "elm-audio-player" ]
+        [ audio
+            [ src model.mediaUrl
+            , id "audio-player"
+            ]
+            []
+        ]
+
+main : Program Never Model Msg
 main =
-        span [ class "welcome-message" ] [ text "Hello, World!" ]
+    Html.program
+        { init = ( initialModel, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = (\model -> Sub.none )
+        }
