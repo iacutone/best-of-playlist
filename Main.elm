9de2b45d9681383f445a054f6d3cec378aa5d9df
Eric Iacutone <eric.iacutone@gmail.com>
2017-04-03 20:06:05 -0400

    Implement mvp

--- Main.elm
+++ Main.elm
@@ -3,10 +3,11 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on, onClick)
-import Time
 import Debug
 import Port
 import Json.Decode as Decode exposing (..)
+import Svg exposing (svg, circle)
+import Svg.Attributes exposing (cx, cy, r, stroke, fill, strokeWidth, strokeDasharray, strokeDashoffset)
 
 -- MSG
 
@@ -16,19 +17,27 @@ type Msg
     | Pause
     | UpdateTime Float
     | SetDuration Float
+    | SetSongId String
+    | SetSongName String
+    | PlayNextSong String
 
 -- MODEL
 
 type alias Model =
     { songs: List Song
-    , currentSong: String
+    , currentSongId: String
+    , currentSongName: String
     , currentTime: Float
     , duration: Float
     , playing: Bool
+    , position: Float
+    , title: String
     }
 
 type alias Flags =
-    { songs: List Song }
+    { songs: List Song
+    , title: String
+    }
 
 type alias Song =
     { songSource: String
@@ -39,23 +48,30 @@ type alias Song =
 initialModel : Flags -> Model
 initialModel flags =
     { songs = flags.songs
-    , currentSong = "song1"
+    , currentSongId = "song1"
+    , currentSongName = ""
     , currentTime = 0.0
     , duration = 0.0
     , playing = False
+    , position = 1
+    , title = flags.title
     }
 
 onTimeUpdate : (Float -> msg) -> Attribute msg
 onTimeUpdate msg =
     on "timeupdate" (Decode.map msg targetCurrentTime)
 
+onEnd : (String -> msg) -> Attribute msg
+onEnd msg =
+    on "ended" (Decode.map msg targetId)
+
 targetCurrentTime : Decoder Float
 targetCurrentTime =
     Decode.at [ "target", "currentTime" ] Decode.float
 
-targetDuration : Decoder Float
-targetDuration =
-    Decode.at [ "target", "duration" ] Decode.float
+targetId : Decoder String
+targetId =
+    Decode.at [ "target", "id" ] Decode.string
 
 -- UPDATE
 
@@ -63,13 +79,25 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Play ->
-            ( { model | playing = True }, Port.play (model.currentSong) )
+            ( { model | playing = True }, Port.play (model.currentSongId) )
         Pause ->
-            ( { model | playing = False }, Port.pause (model.currentSong) )
+            ( { model | playing = False }, Port.pause (model.currentSongId) )
         UpdateTime time ->
             ( { model | currentTime = time }, Cmd.none )
         SetDuration time ->
             ( { model | duration = time }, Cmd.none )
+        SetSongId msg ->
+            ( { model | currentSongId = msg }, Cmd.none )
+        SetSongName msg ->
+            ( { model | currentSongName = msg }, Cmd.none )
+        PlayNextSong msg ->
+            if toFloat (List.length model.songs) == model.position then
+                ( model, Cmd.none )
+            else
+                let
+                    song = "song" ++ toString (model.position + 1)
+                in
+                    ( { model | position = model.position + 1 }, Port.play (song) )
         _ ->
             Debug.log "Unknown message" ( model, Cmd.none )
 
@@ -77,14 +105,40 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [ class "content" ]
-        [ viewSongs model.songs
-        , footer model
+    div [ class "container content" ]
+        [ div [ class "row" ]
+            [ div [ class "three columns" ] [],
+                div [ class "six columns" ]
+                [ viewTitle model.title
+                , viewSongs model.songs
+                , songActions model
+                ],
+            div [ class "three columns" ] []
+            ]
         ]
+
+viewCircle : Model -> Html Msg
+viewCircle model =
+    let
+        rInt = 35
+        radius = toString rInt
+        cInt = 3.14 * (rInt*2)
+        circumference = toString cInt
+        percentComplete = ((model.duration - model.currentTime) / model.duration) * 100
+        pctInt = ((100-percentComplete)/100)*cInt
+        pct = toString pctInt
+    in
+        svg [ id "circle", height 200,  width 100 ]
+            [ circle [ id "circle-stroke", cx "40", cy "40", r radius, stroke "#FA5B19", strokeWidth "3", fill "transparent", strokeDasharray circumference, strokeDashoffset pct] []
+            ]
 
 viewSongs : List Song -> Html Msg
 viewSongs songs =
     ul [] (List.map audioSong songs)
+
+viewTitle : String -> Html Msg
+viewTitle title =
+    h2 [ id "playlist-title" ] [ text title ]
 
 audioSong : Song -> Html Msg
 audioSong song =
@@ -92,7 +146,9 @@ audioSong song =
         [ text song.songName
         , audio
             [ onTimeUpdate UpdateTime
+            , onEnd PlayNextSong
             , src song.songSource
+            , name song.songName
             , id song.id
             ]
             []
@@ -102,38 +158,24 @@ viewPlayButton : Bool -> Html Msg
 viewPlayButton playing =
     if playing then
         button
-            [ class "fa fa-pause pause"
+            [ class "fa fa-pause btn-circle pause"
             , name "pause"
             , onClick Pause
             ]
             []
     else
         button
-            [ class "fa fa-play play"
+            [ class "fa fa-play btn-circle play"
             , name "play"
             , onClick Play
             ]
             []
 
-viewSongName : String -> Html Msg
-viewSongName name =
-    div [] [ text name ]
-
-viewSongCurrentTime : Float -> Html Msg
-viewSongCurrentTime time =
-    div [] [ text (toString time) ]
-
-viewSongDuration : Float -> Html Msg
-viewSongDuration duration =
-    div [] [ text (toString duration) ]
-
-footer : Model -> Html Msg
-footer model =
-    div []
+songActions : Model -> Html Msg
+songActions model =
+    div [ id "song-action-container" ]
         [ viewPlayButton model.playing
-        , viewSongName model.currentSong
-        , viewSongDuration model.duration
-        , viewSongCurrentTime model.currentTime
+        , viewCircle model
         ]
 
 init : Flags -> (Model, Cmd Msg)
@@ -143,7 +185,11 @@ init flags =
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Port.setDuration SetDuration
+    Sub.batch
+        [ Port.setDuration SetDuration
+        , Port.setSongId SetSongId
+        , Port.setSongName SetSongName
+        ]
 
 main : Program Flags Model Msg
 main =
--- Port.elm
+++ Port.elm
@@ -3,3 +3,5 @@ port module Port exposing (..)
 port play : (String) -> Cmd msg
 port pause : (String) -> Cmd msg
 port setDuration : (Float -> msg) -> Sub msg
+port setSongId : (String -> msg) -> Sub msg
+port setSongName : (String -> msg) -> Sub msg
